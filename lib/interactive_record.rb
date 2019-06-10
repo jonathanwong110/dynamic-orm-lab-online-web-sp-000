d492c9e60edab8d5d5f20217cae9561865369df5
Jonathan Wong <jonathan.wong110@gmail.com>
2019-06-10 21:10:24 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -58,7 +58,7 @@ class InteractiveRecord
   end
   
   def self.find_by(hash)
-    sql = "SELECT * FROM #{self.table_name} WHERE #{array.keys[0].to_s} = '#{array.values[0].to_s}'"
+    sql = "SELECT * FROM #{self.table_name} WHERE #{hash.keys[0].to_s} = '#{hash.values[0].to_s}'"
     DB[:conn].execute(sql)
   end
 
